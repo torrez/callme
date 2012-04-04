98ef8427a4beb57a3faefda8f499417eb625c979
torrez <andre@torrez.org>
2012-04-03 22:35:08 -0700

    Initial commit.

---
+++ conference.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+from twilio.rest import TwilioRestClient
+from twilio.rest.resources import Conferences
+import settings 
+
+
+client = TwilioRestClient(settings.API_KEY, settings.API_TOK)
+
+parser = argparse.ArgumentParser(description='Call some people for a conference. Be sure you’ve set up your Twilio dealio.')
+parser.add_argument('numbers', metavar='N', type=str, nargs='+',
+                   help='Phone numbers to call into a conference.')
+
+args = parser.parse_args()
+numbers = vars(args)['numbers']
+
+for number in numbers:
+	call = client.calls.create(to=number,  
+	                           from_=settings.FROM_NUMBER,
+	                           url=settings.CONFERENCE_URL)	
---
+++ settings.example.py
@@ -0,0 +1,17 @@
+API_KEY = "" #FROM TWILIO
+API_TOK = "" #FROM TWILIO
+FROM_NUMBER = "+12125551212" #Your Twilio number
+CONFERENCE_URL = "http://torrez.org/conference"
+#put a URL here that will respond to a POST with:
+#
+# <?xml version="1.0" encoding="UTF-8"?>
+# <Response>
+#	<Say>Joining a conference room</Say>
+#	<Dial>
+#		<Conference>My Room</Conference>
+#	</Dial>
+# </Response>
+# 
+# Feel free to use my URL for as long as 
+# I can remember not to delete it.
+#
